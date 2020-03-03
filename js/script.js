d0053fe2522b54455ae1564edf7a59616f22914d
Ricky Hentama <rickyhendrapratama4@gmail.com>
2020-03-04 06:44:44 +0700

    change

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-const initiGaleri = document.querySelector('.inti-galeri');
-const jumbo = document.querySelector('.jumbo');
+const initiGaleri = document.querySelector(".inti-galeri");
+const jumbo = document.querySelector(".jumbo");
 
-initiGaleri.addEventListener('click',function(e){
-	if(e.target.className == 'mini'){
-		console.log(jumbo.src = e.target.src);
-	}
-})
+initiGaleri.addEventListener("click", function(e) {
+  if (e.target.className == "mini") {
+    console.log((jumbo.src = e.target.src));
+  }
+});
 
+$(".about").mouseenter(function() {
+  $("section div .lorem").fadeIn(750);
+});
+$(".about").mouseleave(function() {
+  $("section div .lorem").fadeOut(1000);
+});
